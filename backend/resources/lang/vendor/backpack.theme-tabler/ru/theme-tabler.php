43c6b73be37b213a289abcda6be09ddea4cfc8a5
BmpCorp <bmpcorp9@gmail.com>
2025-09-26 15:22:56 +0700

    fix: backpack localization fixes

---
+++ backend/resources/lang/vendor/backpack.theme-tabler/ru/theme-tabler.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // color mode
+    'color-mode-description' => ':mode тема активна',
+    'color-mode-system' => 'Системная',
+    'color-mode-light' => 'Светлая',
+    'color-mode-dark' => 'Тёмная',
+
+    // password
+    'password-show' => 'Показать пароль',
+    'password-hide' => 'Скрыть пароль',
+];
