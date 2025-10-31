a80b5b426a1806375b2abfe852b0d63b0781cd67
BmpCorp <bmpcorp9@gmail.com>
2025-10-31 19:43:32 +0700

    feat: api profiling with debugbar

---
+++ backend/app/Http/Middleware/ProfileApiResponse.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProfileApiResponse
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Closure(Request): (Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        if (!app()->isProduction() && app()->hasDebugModeEnabled()) {
+            $debugbarData = app('debugbar')->getData();
+            $responseData = json_decode($response->getContent(), true);
+
+            $responseData['debugbar'] = [
+                'id' => $debugbarData['__meta']['id'] ?? null,
+                'time' => $debugbarData['time']['duration'] ?? null,
+                'memory' => $debugbarData['memory']['peak_usage'] ?? null,
+                'queries_count' => $debugbarData['queries']['count'] ?? null,
+                'queries' => is_array($debugbarData['queries']['statements'])
+                    ? array_map(fn ($statement) => $statement['sql'], $debugbarData['queries']['statements'])
+                    : [],
+                'models_count' => $debugbarData['models']['badges'] ?? null,
+            ];
+
+            $response->setContent(json_encode($responseData));
+        }
+
+        return $response;
+    }
+}
