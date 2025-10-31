9f0f84bb26fdd007d2a6d1f8dd3e77a91937b92c
BmpCorp <bmpcorp9@gmail.com>
2025-10-31 21:28:00 +0700

    fix: prevent testing with cached config

--- backend/app/Http/Middleware/ProfileApiResponse.php
+++ backend/app/Http/Middleware/ProfileApiResponse.php
@@ -21,14 +21,18 @@ class ProfileApiResponse
             $debugbarData = app('debugbar')->getData();
             $responseData = json_decode($response->getContent(), true);
 
+            if (isset($debugbarData['queries']['statements']) && is_array($debugbarData['queries']['statements'])) {
+                $queries = array_map(fn ($statement) => $statement['sql'], $debugbarData['queries']['statements']);
+            } else {
+                $queries = [];
+            }
+
             $responseData['debugbar'] = [
                 'id' => $debugbarData['__meta']['id'] ?? null,
                 'time' => $debugbarData['time']['duration'] ?? null,
                 'memory' => $debugbarData['memory']['peak_usage'] ?? null,
                 'queries_count' => $debugbarData['queries']['count'] ?? null,
-                'queries' => is_array($debugbarData['queries']['statements'])
-                    ? array_map(fn ($statement) => $statement['sql'], $debugbarData['queries']['statements'])
-                    : [],
+                'queries' => $queries,
                 'models_count' => $debugbarData['models']['badges'] ?? null,
             ];
 
